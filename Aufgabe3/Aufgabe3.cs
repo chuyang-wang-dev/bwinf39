eb0656c2122b75df933c654d829118f1771f473d
ChuangSheep <44814054+ChuangSheep@users.noreply.github.com>
2020-09-19 14:40:40 +0200

    Finished Aufg. 3 and 5. Changed build target to win-x64

---
+++ Aufgabe3/Aufgabe3.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.IO;
+
+namespace Aufgabe3
+{
+  public class Turnier
+  {
+    public static void Main(string[] args)
+    {
+      RNGProvider rng = new RNGProvider();
+      Console.WriteLine("Bitte Name der Test-Datei eingeben...");
+      string testFilePath = Console.ReadLine();
+
+      if (File.Exists(testFilePath))
+      {
+        using (StreamReader sr = File.OpenText(testFilePath))
+        {
+          int playerCount = Convert.ToInt32(sr.ReadLine());
+
+          // int[i] = Spielstaerke der SpielerIn i+1
+          int[] playerStrength = new int[playerCount];
+          for (int i = 0; i < playerCount; i++)
+          {
+            playerStrength[i] = Convert.ToInt32(sr.ReadLine());
+          }
+
+          int bestPlayerNum;
+          double procent;
+
+          KOx1 koX1 = new KOx1(playerStrength, rng, new int[] {
+            0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+          });
+          koX1.PlayGameNth(1000000);
+          procent = koX1.GetWinningProcentOfBestPlayer(out bestPlayerNum);
+          Console.WriteLine($"{bestPlayerNum}: {procent}");
+
+          KOx5 koX5 = new KOx5(playerStrength, rng, new int[] {
+            0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+          });
+          koX5.PlayGameNth(1000000);
+          procent = koX5.GetWinningProcentOfBestPlayer(out bestPlayerNum);
+          Console.WriteLine($"{bestPlayerNum}: {procent}");
+
+          Liga liga = new Liga(playerStrength, rng);
+          liga.PlayGameNth(1000000);
+          procent = liga.GetWinningProcentOfBestPlayer(out bestPlayerNum);
+          Console.WriteLine($"{bestPlayerNum}: {procent}");
+
+
+        }
+      }
+      else
+      {
+        Console.WriteLine("Die gegebene Datei-Namen ist nicht gueltig. Das Programm und die Datei muessen in demselben Ordner stehen. ");
+      }
+      Console.ReadKey();
+    }
+
+    private class Liga : GameModel
+    {
+      public Liga(int[] playerStrengths, RNGProvider rngProvider)
+      {
+        _playerStrengths = playerStrengths;
+        _winTimesTotal = new int[playerStrengths.Length];
+        // Set default val to 0
+        Array.Clear(_winTimesTotal, 0, _winTimesTotal.Length);
+        _rngProvider = rngProvider;
+      }
+
+      public override void PlayGameNth(int n)
+      {
+        for (int i = 0; i < n; i++)
+        {
+          PlayGameOnce();
+        }
+      }
+
+      public override void PlayGameOnce()
+      {
+        int[] winTimes = new int[_playerStrengths.Length];
+        // Set default val to 0
+        Array.Clear(winTimes, 0, winTimes.Length);
+        // Array index = the player number - 1
+        for (int playerNumber = 0; playerNumber < _playerStrengths.Length - 1; playerNumber++)
+        {
+          for (int againstPlayerNum = playerNumber + 1; againstPlayerNum < _playerStrengths.Length; againstPlayerNum++)
+          {
+            if (_rngProvider.GetIntBetween(1, _playerStrengths[againstPlayerNum] + _playerStrengths[playerNumber]) <= _playerStrengths[playerNumber])
+            {
+              // 1. Player wins
+              winTimes[playerNumber] += 1;
+            }
+            else
+            {
+              // 2. Player wins
+              winTimes[againstPlayerNum] += 1;
+            }
+          }
+        }
+        int highstScorePlayer = -1;
+        int highstScore = -1;
+        for (int i = 0; i < winTimes.Length; i++)
+        {
+          if (winTimes[i] > highstScore)
+          {
+            highstScorePlayer = i;
+            highstScore = winTimes[i];
+          }
+          else if (winTimes[i] == highstScore)
+          {
+            highstScorePlayer = highstScorePlayer < i ? highstScorePlayer : i;
+          }
+        }
+        _winTimesTotal[highstScorePlayer] += 1;
+      }
+    }
+
+
+    private class KOx1 : GameModel
+    {
+      private int[] _playPlan;
+      public KOx1(int[] playerStrengths, RNGProvider rngProvider, int[] playPlan)
+      {
+        _playerStrengths = playerStrengths;
+        _winTimesTotal = new int[playerStrengths.Length];
+        // Set default val to 0
+        Array.Clear(_winTimesTotal, 0, _winTimesTotal.Length);
+        _rngProvider = rngProvider;
+        _playPlan = playPlan;
+      }
+
+      public override void PlayGameNth(int n)
+      {
+        for (int i = 0; i < n; i++)
+        {
+          PlayGameOnce();
+        }
+      }
+
+      public override void PlayGameOnce()
+      {
+        // Initialize the tree
+        int[] initData = new int[_playPlan.Length * 2 - 1];
+        for (int i = 0; i < initData.Length; i++) initData[i] = -1;
+        _playPlan.CopyTo(initData, _playPlan.Length - 1);
+        // Stores the play plan
+        Node<int> treeRoot = new CompleteBinaryTreeBuilder<int>(initData).Root;
+        GetGameResult(treeRoot);
+        _winTimesTotal[treeRoot.Data] += 1;
+      }
+
+      private void GetGameResult(Node<int> currentNode)
+      {
+        if (currentNode.RightChild != null)
+        {
+          GetGameResult(currentNode.LeftChild);
+          GetGameResult(currentNode.RightChild);
+          currentNode.Data = GetWinner(currentNode.LeftChild, currentNode.RightChild);
+        }
+        else
+        {
+          currentNode.Parent.Data = GetWinner(currentNode.Parent.LeftChild, currentNode.Parent.RightChild);
+        }
+      }
+      private int GetWinner(Node<int> player1, Node<int> player2)
+      {
+        if (_rngProvider.GetIntBetween(1, _playerStrengths[player1.Data] + _playerStrengths[player2.Data]) <= _playerStrengths[player1.Data])
+        {
+          return player1.Data;
+        }
+        else return player2.Data;
+      }
+    }
+
+    private class KOx5 : GameModel
+    {
+      private int[] _playPlan;
+      public KOx5(int[] playerStrengths, RNGProvider rngProvider, int[] playPlan)
+      {
+        _playerStrengths = playerStrengths;
+        _winTimesTotal = new int[playerStrengths.Length];
+        // Set default val to 0
+        Array.Clear(_winTimesTotal, 0, _winTimesTotal.Length);
+        _rngProvider = rngProvider;
+        _playPlan = playPlan;
+      }
+      public override void PlayGameOnce()
+      {
+        // Initialize the tree
+        int[] initData = new int[_playPlan.Length * 2 - 1];
+        for (int i = 0; i < initData.Length; i++) initData[i] = -1;
+        _playPlan.CopyTo(initData, _playPlan.Length - 1);
+        // Stores the play plan
+        Node<int> treeRoot = new CompleteBinaryTreeBuilder<int>(initData).Root;
+        GetGameResult(treeRoot);
+        _winTimesTotal[treeRoot.Data] += 1;
+      }
+
+      private void GetGameResult(Node<int> currentNode)
+      {
+        if (currentNode.RightChild != null)
+        {
+          GetGameResult(currentNode.LeftChild);
+          GetGameResult(currentNode.RightChild);
+          currentNode.Data = GetWinner(currentNode.LeftChild, currentNode.RightChild);
+        }
+        else
+        {
+          currentNode.Parent.Data = GetWinner(currentNode.Parent.LeftChild, currentNode.Parent.RightChild);
+        }
+      }
+      private int GetWinner(Node<int> player1, Node<int> player2)
+      {
+        int[] winningTimeCount = new int[2];
+        Array.Clear(winningTimeCount, 0, 2);
+        for (int i = 0; i < 5; i++)
+        {
+          if (_rngProvider.GetIntBetween(1, _playerStrengths[player1.Data] + _playerStrengths[player2.Data]) <= _playerStrengths[player1.Data]) winningTimeCount[0]++;
+          else winningTimeCount[1]++;
+        }
+        return winningTimeCount[0] > winningTimeCount[1] ? player1.Data : player2.Data;
+      }
+
+      public override void PlayGameNth(int n)
+      {
+        for (int i = 0; i < n; i++)
+        {
+          PlayGameOnce();
+        }
+      }
+    }
+
+    private abstract class GameModel
+    {
+      protected int[] _winTimesTotal;
+      protected int[] _playerStrengths;
+      protected RNGProvider _rngProvider;
+      public abstract void PlayGameOnce();
+      public abstract void PlayGameNth(int n);
+      // overload
+      public double GetWinningProcentOfBestPlayer(out int bestPlayerNumber)
+      {
+        bestPlayerNumber = 0;
+        for (int i = 0; i < _playerStrengths.Length; i++)
+        {
+          if (_playerStrengths[i] > _playerStrengths[bestPlayerNumber]) bestPlayerNumber = i;
+        }
+        int totalTimes = 0;
+        foreach (var i in _winTimesTotal)
+        {
+          totalTimes += i;
+        }
+        return (double)_winTimesTotal[bestPlayerNumber] / (double)totalTimes;
+      }
+      public double GetWinningProcentOfBestPlayerO(out int bestPlayerNumber)
+      {
+        bestPlayerNumber = -1;
+        int mostWinTimes = -1, totalTimes = 0;
+        for (int i = 0; i < _winTimesTotal.Length; i++)
+        {
+          totalTimes += _winTimesTotal[i];
+          if (_winTimesTotal[i] > mostWinTimes)
+          {
+            mostWinTimes = _winTimesTotal[i];
+            bestPlayerNumber = i;
+          }
+          else if (_winTimesTotal[i] == mostWinTimes)
+          {
+            bestPlayerNumber = bestPlayerNumber < i ? bestPlayerNumber : i;
+          }
+        }
+        return (double)mostWinTimes / (double)totalTimes;
+      }
+    }
+
+    private class Node<T>
+    {
+      public T Data { get; set; }
+      public Node<T> LeftChild { get; set; }
+      public Node<T> RightChild { get; set; }
+      public Node<T> Parent { get; set; }
+
+      public Node(T data)
+      {
+        this.Data = data;
+      }
+
+    }
+
+    private class CompleteBinaryTreeBuilder<T>
+    {
+      public Node<T> Root { get; }
+      public CompleteBinaryTreeBuilder(params T[] dataArray)
+      {
+        Root = build(dataArray, 0, null);
+      }
+
+      private Node<T> build(T[] arr, int currentIndex, Node<T> parentNode)
+      {
+        if (currentIndex < arr.Length)
+        {
+          Node<T> currentNode = new Node<T>(arr[currentIndex]);
+          currentNode.Parent = parentNode;
+          currentNode.LeftChild = build(arr, currentIndex * 2 + 1, currentNode);
+          currentNode.RightChild = build(arr, currentIndex * 2 + 2, currentNode);
+          return currentNode;
+        }
+        return null;
+      }
+    }
+
+    private class RNGProvider
+    {
+      private System.Random rng = new System.Random();
+
+      // i1 and i2 are inclusive
+      public int GetIntBetween(int i1, int i2)
+      {
+        if (i1 < 0 || i1 > i2) throw new ArgumentException(nameof(i1));
+        return rng.Next(i1, i2 + 1);
+      }
+    }
+  }
+}
---
+++ Aufgabe5/Aufgabe5.cs
@@ -0,0 +1,266 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aufgabe5
+{
+  public static class WichtelnSortierment
+  {
+    private static bool Contains(this int[] arr, int val)
+    {
+      foreach (int i in arr)
+      {
+        if (i == val) return true;
+      }
+      return false;
+    }
+
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Bitte Name der Test-Datei eingeben...");
+      string testFilePath = Console.ReadLine();
+
+      if (File.Exists(testFilePath))
+      {
+        using (StreamReader sr = File.OpenText(testFilePath))
+        {
+          int studentCount = Convert.ToInt32(sr.ReadLine());
+
+          List<Person> students = new List<Person>(studentCount);
+          for (int i = 0; i < studentCount; i++)
+          {
+            string[] line = sr.ReadLine().Trim().Split(' ');
+            int[] wishesWithIndexI = new int[3];
+            for (int j = 0, wishI = 0; j < line.Length; j++)
+            {
+              if (!string.IsNullOrWhiteSpace(line[j])) wishesWithIndexI[wishI++] = Convert.ToInt32(line[j].Trim()) - 1;
+            }
+            students.Add(new Person(i, wishesWithIndexI));
+          }
+
+          int[] givenGiftsFromCol0 = GetAllGivenGiftInCol(0, students, new List<Wish>());
+
+          // key: gift number
+          // val: person who has it as index
+          Dictionary<int, int> giftsTo = new Dictionary<int, int>();
+
+          // Inspect the 0. col and take the indentical gift wish
+          processCol(0, ref students, ref giftsTo, givenGiftsFromCol0);
+          processCol(1, ref students, ref giftsTo, giftsTo.Keys.ToArray());
+          processCol(2, ref students, ref giftsTo, giftsTo.Keys.ToArray());
+
+          Dictionary<int, int> personWithGift = new Dictionary<int, int>();
+          for (int i = 0; i < studentCount; i++)
+          {
+            int personIndex;
+            if (giftsTo.TryGetValue(i, out personIndex)) personWithGift.Add(personIndex, i);
+            else
+            {
+              for (int j = 0; j < students.Count; j++)
+              {
+                if (students[j].GotGift == -1)
+                {
+                  students[j].GotGift = i;
+                  giftsTo.Add(i, students[j].Index);
+                  personWithGift.Add(students[j].Index, i);
+                  break;
+                }
+              }
+            }
+          }
+
+          for (int i = 0; i < studentCount; i++)
+          {
+            Console.WriteLine($"Schueler {i + 1} bekommt {personWithGift[i] + 1}");
+          }
+        }
+      }
+      else
+      {
+        Console.WriteLine("Die gegebene Datei-Namen ist nicht gueltig. Das Programm und die Datei muessen in demselben Ordner stehen. ");
+      }
+      Console.ReadKey();
+    }
+
+    private static void processCol(int col, ref List<Person> students, ref Dictionary<int, int> giftsTo, int[] givenGifts)
+    {
+      int studentCount = students.Count;
+      for (int i = 0; i < studentCount; i++)
+      {
+        int[] allPersonWithSameWish = GetAllPersonIndexWithSameGiftWish(students[i].Wishes[col], students, col);
+        if (allPersonWithSameWish.Length == 1 && allPersonWithSameWish[0] == i)
+        {
+          giftsTo.Add(students[i].Wishes[col].GiftWish, students[i].Index);
+          students[i].GotGift = students[i].Wishes[col].GiftWish;
+          students[i].SetAllWishToUnable();
+        }
+      }
+      // change the avaliable attribute of all person's wishes
+      for (int idx = 0; idx < studentCount; idx++)
+      {
+        students[idx].CheckAllWishesAvaliblility(givenGifts.Union(giftsTo.Keys).ToArray(), col + 1);
+      }
+
+      // Check the gift option that comes more than once
+      for (int i = 0; i < studentCount; i++)
+      {
+        if (students[i].Wishes[col].Avaliable)
+        {
+          int[] allPersonWithSameWish = GetAllPersonIndexWithSameGiftWish(students[i].Wishes[col], students, col);
+          double lowestScore = double.MaxValue;
+          int personIndexWithLowestScore = -1;
+
+          for (int j = 0; j < allPersonWithSameWish.Length; j++)
+          {
+            if (students[allPersonWithSameWish[j]].Calculate(students, col) < lowestScore)
+            {
+              lowestScore = students[allPersonWithSameWish[j]].Calculate(students, col);
+              personIndexWithLowestScore = allPersonWithSameWish[j];
+            }
+          }
+
+          giftsTo.Add(students[i].Wishes[col].GiftWish, personIndexWithLowestScore);
+          students[personIndexWithLowestScore].GotGift = students[i].Wishes[col].GiftWish;
+          students[personIndexWithLowestScore].SetAllWishToUnable();
+          // change the avaliable attribute of all person's wishes
+          for (int idx = 0; idx < studentCount; idx++)
+          {
+            students[idx].CheckAllWishesAvaliblility(giftsTo.Keys.ToArray(), col);
+          }
+        }
+      }
+    }
+
+    private static int[] GetAllPersonIndexWithSameGiftWish(Wish giftWish, List<Person> students, int col)
+    {
+      List<int> persons = new List<int>();
+      for (int i = 0; i < students.Count; i++)
+      {
+        if (students[i].Wishes[col].GiftWish == giftWish.GiftWish && students[i].Wishes[col].Avaliable) persons.Add(students[i].Index);
+      }
+      return persons.ToArray();
+    }
+
+    //private static int[] GetAllSameGiftWishWithPersonIndex(Person person, List<Person> students, int col)
+
+
+    private static int[] GetAllGivenGiftInCol(int col, List<Person> students, List<Wish> except)
+    {
+      List<int> giftNums = new List<int>();
+      for (int i = 0; i < students.Count; i++)
+      {
+        if (!giftNums.Contains(students[i].Wishes[col].GiftWish) && !except.Contains(students[i].Wishes[col])) giftNums.Add(students[i].Wishes[col].GiftWish);
+      }
+      return giftNums.ToArray();
+    }
+
+    private class Wish
+    {
+      public int GiftWish
+      {
+        get;
+      }
+
+      public bool Avaliable
+      {
+        get;
+        set;
+      }
+
+      public int Col
+      {
+        get;
+      }
+
+      public Wish(int person, int wish, int col)
+      {
+        Avaliable = true;
+        GiftWish = wish;
+        Col = col;
+      }
+
+      public static Wish[] ConvertToWishes(int[] wishesOfOnePerson, int personIndex)
+      {
+        return new Wish[] { new Wish(personIndex, wishesOfOnePerson[0], 0), new Wish(personIndex, wishesOfOnePerson[1], 1), new Wish(personIndex, wishesOfOnePerson[2], 2) };
+      }
+    }
+
+    private class Person
+    {
+      public int Index
+      {
+        get;
+      }
+
+      public Wish[] Wishes
+      {
+        get;
+      }
+
+      public Wish Wish1
+      {
+        get { return Wishes[0]; }
+      }
+
+      public Wish Wish2
+      {
+        get { return Wishes[1]; }
+      }
+
+      public Wish Wish3
+      {
+        get { return Wishes[2]; }
+      }
+
+      public int GotGift
+      {
+        get; set;
+      }
+
+      public Person(int index, int[] wishes)
+      {
+        Index = index;
+        Wishes = Wish.ConvertToWishes(wishes, index);
+        GotGift = -1;
+      }
+
+      public void SetAllWishToUnable()
+      {
+        for (int i = 0; i < 3; i++) this.Wishes[i].Avaliable = false;
+      }
+
+      public void SetAvaliableOfWish(int wishCol, bool avaliable = false)
+      {
+        Wishes[wishCol].Avaliable = avaliable;
+      }
+
+      // If any wish is in the given array, change its status to unavaliable
+      public void CheckAllWishesAvaliblility(int[] givenGift, int startCol)
+      {
+        for (int i = startCol; i < 3; i++)
+        {
+          if (givenGift.Contains(Wishes[i].GiftWish)) Wishes[i].Avaliable = false;
+        }
+      }
+      public double Calculate(List<Person> students, int col)
+      {
+        double score1 = 0;
+        double score2 = 0;
+
+        if (col == 0 && this.Wishes[1].Avaliable)
+        {
+          score1 = (double)10000 / (double)GetAllPersonIndexWithSameGiftWish(this.Wishes[1], students, 1).Length;
+          score1 -= score1 - GetAllPersonIndexWithSameGiftWish(this.Wishes[1], students, 2).Length > 0 ? GetAllPersonIndexWithSameGiftWish(this.Wishes[1], students, 2).Length : score1;
+        }
+        if (col < 2 && this.Wishes[2].Avaliable)
+        {
+          score2 = (double)100 / (double)GetAllPersonIndexWithSameGiftWish(this.Wishes[2], students, 2).Length;
+          int appearTimesIn1 = GetAllPersonIndexWithSameGiftWish(this.Wishes[2], students, 1).Length;
+          score2 = score2 - 10 * appearTimesIn1 > 1 ? score2 - 10 * appearTimesIn1 : 1;
+        }
+        return score1 + score2;
+      }
+    }
+  }
+}
